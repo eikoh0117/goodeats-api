ab3fad6e141ca163bca5d9ee4b9f17ac1359a669
Hidemitsu Aoki <olympos0117@gmail.com>
2021-01-23 18:04:20 +0900

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source 'https://rubygems.org'
+
+gem 'aws-record', '~> 2.0'
+gem 'dotenv'
+gem 'addressable'
---
+++ lambda_function.rb
@@ -0,0 +1,9 @@
+require 'net/http'
+require 'uri'
+require 'json'
+require 'addressable/uri'
+require 'dotenv/load'
+require 'aws-record'
+
+def lambda_handler(event:, context:)
+end
